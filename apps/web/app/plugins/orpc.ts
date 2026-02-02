9f59365a2b7a0d894995dd0c5581b013d8d251cb
raphdcst <rdecostaire@gmail.com>
2026-02-02 23:01:43 +0100

    initial commit

---
+++ apps/web/app/plugins/orpc.ts
@@ -0,0 +1,30 @@
+import type { AppRouterClient } from "@la-brocante-scoute/api/routers/index";
+
+import { defineNuxtPlugin } from "#app";
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+
+export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig();
+  const rpcUrl = `${config.public.serverUrl}/rpc`;
+
+  const rpcLink = new RPCLink({
+    url: rpcUrl,
+    fetch(url, options) {
+      return fetch(url, {
+        ...options,
+        credentials: "include",
+      });
+    },
+  });
+
+  const client: AppRouterClient = createORPCClient(rpcLink);
+  const orpcUtils = createTanstackQueryUtils(client);
+
+  return {
+    provide: {
+      orpc: orpcUtils,
+    },
+  };
+});
