10f1f0958937f2fb69d135896c195c505156b71d
raphdcst <rdecostaire@gmail.com>
2026-02-05 11:44:04 +0100

    feat(api): enable client-side validations for procedure input/output

--- apps/web/app/plugins/orpc.ts
+++ apps/web/app/plugins/orpc.ts
@@ -1,8 +1,9 @@
 import type { AppRouterClient } from "@la-brocante-scoute/api/routers/index";
-
+import { appContract } from "@la-brocante-scoute/contract";
 import { defineNuxtPlugin } from "#app";
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
+import { RequestValidationPlugin, ResponseValidationPlugin } from "@orpc/contract/plugins";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 
 export default defineNuxtPlugin(() => {
@@ -10,6 +11,7 @@ export default defineNuxtPlugin(() => {
   const rpcUrl = `${config.public.serverUrl}/api/rpc`;
 
   const rpcLink = new RPCLink({
+    plugins: [new RequestValidationPlugin(appContract), new ResponseValidationPlugin(appContract)],
     url: rpcUrl,
     fetch(url, options) {
       return fetch(url, {
