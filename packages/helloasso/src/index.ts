977853c2a6a4bdbea764ef2d26831159235e6d88
raphdcst <rdecostaire@gmail.com>
2026-02-13 19:18:50 +0100

    chore(helloasso): init and export client

--- packages/helloasso/src/index.ts
+++ packages/helloasso/src/index.ts
@@ -1,1 +1,8 @@
-console.log("helloasso pkg");
+import { env } from "@la-brocante-scoute/env/server";
+import { HelloAssoClient } from "@lecoq/helloasso-sdk";
+
+export const client = new HelloAssoClient({
+  clientId: env.HELLOASSO_CLIENT_ID,
+  clientSecret: env.HELLOASSO_CLIENT_SECRET,
+  defaultResponseType: "json",
+});
