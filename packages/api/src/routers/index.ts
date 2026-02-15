d610d20982de4d88e7e1acbff1db2b2cc496b81b
raphdcst <rdecostaire@gmail.com>
2026-02-16 00:45:03 +0100

    refactor(payment): remove stripe endpoints

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,12 +1,10 @@
 import type { RouterClient } from "@orpc/server";
 import { oc } from "..";
 import { healthRouter } from "./health";
-import { stripeRouter } from "./stripe";
 import { testRouter } from "./test";
 
 export const appRouter = oc.router({
   health: healthRouter,
-  stripe: stripeRouter,
   test: testRouter,
 });
 
--- packages/contract/src/contracts/index.ts
+++ packages/contract/src/contracts/index.ts
@@ -1,9 +1,7 @@
 import { healthGlobalContract } from "./health";
-import { stripeGlobalContract } from "./stripe";
 import { testGlobalContract } from "./test";
 
 export const appContract = {
   health: healthGlobalContract,
-  stripe: stripeGlobalContract,
   test: testGlobalContract,
 };
