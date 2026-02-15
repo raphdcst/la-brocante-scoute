7bc868db4a2d4785a109f2afe17ad9a518fb02f2
raphdcst <rdecostaire@gmail.com>
2026-02-16 00:33:49 +0100

    refactor(payment): remove outdated pkgs

--- packages/helloasso/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "@la-brocante-scoute/env/server";
-import { HelloAssoClient } from "@lecoq/helloasso-sdk";
-
-export const client = new HelloAssoClient({
-  clientId: env.HELLOASSO_CLIENT_ID,
-  clientSecret: env.HELLOASSO_CLIENT_SECRET,
-  defaultResponseType: "json",
-});
--- packages/stripe/src/index.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-import { env } from "@la-brocante-scoute/env/server";
-
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2026-01-28.clover",
-});
-
-export * from "./types";
